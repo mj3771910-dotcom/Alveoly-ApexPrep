ec7ae549be48cdee3af3fa3e0de7a63b660a2404
Bofah Emmanuel <mj3771910@gmail.com@gmail.com>
2026-03-25 16:34:55 +0000

    feat: update feature

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,7 +1,7 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../src/models/User.js";
-import generateToken from "../utils/generateToken.js";
+import generateToken from "../src/utils/generateToken.js";
 
 passport.use(
   new GoogleStrategy(
