9f1db77651e69185623c96ecd556b0500ee5a86a
Bofah Emmanuel <mj3771910@gmail.com@gmail.com>
2026-03-27 16:08:04 +0000

    feat: update feature

--- frontend/src/pages/StudentDashboard.jsx
+++ frontend/src/pages/StudentDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Joyride from "react-joyride"; // ✅ NEW
+import { Joyride } from "react-joyride"; // ✅ NEW
 import { useNavigate } from "react-router-dom";
 import {
   FaBook,
