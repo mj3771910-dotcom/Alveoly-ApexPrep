f3f21428ca73a8bbbc500440a587250c7c0b9249
Bofah Emmanuel <mj3771910@gmail.com@gmail.com>
2026-03-28 12:46:22 +0000

    feat: update feature

--- backend/src/routes/contentPaymentRoutes.js
+++ backend/src/routes/contentPaymentRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { initiateContentPayment } from "../controllers/contentPaymentController.js";
-import protect from "../middleware/authMiddleware.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
